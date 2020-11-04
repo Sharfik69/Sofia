7d287da3a09606ab90e034568155e217fcf4e8b4
Dmitry Zaguzin <cindersstone@gmail.com>
2020-11-04 20:30:32 +0800

    Добавил небольшой комент

--- Sofia/Sofia/test_system/views.py
+++ Sofia/Sofia/test_system/views.py
@@ -4,5 +4,9 @@ from django.core import serializers
 import json
 
 def test(request):
+    #Здесь пишем логику, сбор данные и все такое
+    #Все данные, которые нам нужно отдать на страницу мы забиваем в словарь и передаем
+    #3 параметром в render (Сейчас там просто пустой массив)
+    #Как это работает, покажу потом
     return render(request, 'user_page/index.html', {})
     #return HttpResponse('<h1> Ti sosal chlen </h1>')
