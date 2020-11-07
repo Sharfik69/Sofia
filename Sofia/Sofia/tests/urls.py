4cdcd1fc6c23ceb94a03730e6673fd0d890641fc
Cherepanov Igor <zigorewslike@gmail.com>
2020-11-08 05:18:01 +0800

    Редактирование

--- Sofia/Sofia/tests/urls.py
+++ Sofia/Sofia/tests/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     # path('cd/<id_test>', views.take_the_test),
     path('t/<id_test>', views.take_the_test),
-    # path('t/<id_test>', views.edit_the_test),
+    # path('e/<id_test>', views.edit_the_test),
     path('cp_post/<id_test>', views.post_the_test),
     path('cd_post/<id_test>', views.post_the_result),
 ]
